6a5d39b188943bd603b79a85c587a15d8c4cc131
Ria-D <92781900+Ria-D@users.noreply.github.com>
2021-12-02 10:21:34 +0000

    Update Exercise_7.py

--- Exercise_7.py
+++ Exercise_7.py
@@ -11,3 +11,51 @@ Use the Vehicle class of the previous exercise (6). Add the following methods to
 
 Weight: 2
 '''
+
+import numpy as np
+
+
+class Vehicle:
+    def __init__(self, type: str, velocity: tuple, colour:str, electric:bool):
+
+        if type != "Car" and type != "Truck":
+            print("Vehicle type must be a Car or Truck")
+
+        self.type = type
+        self.velocity = np.array(velocity)
+        self.colour = colour
+        self.electric = electric
+
+    def updateVelocity(self, newVelocity: tuple):
+      self.velocity = np.array(newVelocity)
+
+    def attributes(self):
+      print(f"Vehicle type:{self.type}, Vehicle velocity:{self.velocity}, Vehicle colour:{self.colour}, Vehicle is electric?:{self.electric}")
+
+    def emissions(self, distance:float):
+      if self.electric == False and self.type == "Car":
+        return(10*distance)
+      elif self.electric == False and self.type == "Truck":
+        return(50*distance)
+      elif self.electric ==True and self.type =="Car":
+        return(distance)
+      elif self.electric ==True and self.type =="Truck":
+        return(5*distance)
+
+      
+
+# example to check code works
+v1 = Vehicle("Car", (3,4), "Blue", False)
+
+print(v1.velocity)
+v1.updateVelocity((9,7))
+print(v1.velocity)
+
+v1.attributes()
+print(v1.emissions(10))
+
+
+
+
+
+
