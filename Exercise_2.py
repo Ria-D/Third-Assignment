dfa3c70b32ec73e21039bc87d57271c4db8530ca
Ria-D <92781900+Ria-D@users.noreply.github.com>
2021-12-02 10:21:14 +0000

    Update Exercise_2.py

--- Exercise_2.py
+++ Exercise_2.py
@@ -4,3 +4,10 @@ from 15 to 55 and print all values except the first one and the last one.
 
 Weight: 1
 '''
+import numpy as np
+
+
+vector1 = np.arange(15,55)
+
+output = vector1[1:-1]
+print(output)
