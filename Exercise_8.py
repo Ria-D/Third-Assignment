7405aca671263d27d18cd093f8a539a673c2c7d5
Ria-D <92781900+Ria-D@users.noreply.github.com>
2021-12-02 10:21:44 +0000

    Update Exercise_8.py

--- Exercise_8.py
+++ Exercise_8.py
@@ -12,3 +12,62 @@ hint: divide this exercise in subproblems and write appropriate functions for ea
 
 Weight: 3
 '''
+import numpy as np
+
+
+class Vehicle:
+    def __init__(self, type: str, velocity: tuple, electric:bool):
+
+        if type != "Car" and type != "Truck":
+            print("Vehicle type must be a Car or Truck")
+
+        self.type = type
+        self.velocity = np.array(velocity)
+        self.electric = electric
+
+    def emissions(self, distance:float):
+      if self.electric == False and self.type == "Car":
+        return(10*distance)
+      elif self.electric == False and self.type == "Truck":
+        return(50*distance)
+      elif self.electric ==True and self.type =="Car":
+        return(distance)
+      elif self.electric ==True and self.type =="Truck":
+        return(5*distance)
+
+import random
+electricCars = []
+for i in range(50):
+  electricCars.append(Vehicle("Car", (random.randint(1,100),random.randint(1,100)), True))
+
+nonElectricCars= []
+for i in range(50):
+  nonElectricCars.append(Vehicle("Car", (random.randint(1,100),random.randint(1,100)), False))
+
+electricTrucks= []
+for i in range(50):
+  electricTrucks.append(Vehicle("Truck", (random.randint(1,100),random.randint(1,100)), True))
+
+nonElectricTrucks= []
+for i in range(50):
+  nonElectricTrucks.append(Vehicle("Truck", (random.randint(1,100),random.randint(1,100)), False))
+
+def avgEmission(list1, time:int):
+    
+    sumEmission = 0
+    for item in list1:
+        distanceTravelled = (np.linalg.norm(item.velocity))*time
+        sumEmission += item.emissions(distanceTravelled)
+    averageEmission = sumEmission/50
+
+    return(averageEmission)
+
+print(avgEmission(electricCars, 10))
+print(avgEmission(nonElectricCars, 10))
+print(avgEmission(electricTrucks, 10))
+print(avgEmission(nonElectricTrucks, 10))
+
+        
+        
+
+
